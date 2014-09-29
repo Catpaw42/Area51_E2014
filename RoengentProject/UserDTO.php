612e14844b17708b1b2524cc753be79f639628a4
Runi <Runi@82.211.200.99>
2014-09-29 20:15:47 +0200

    første forsøg

---
+++ RoengentProject/UserDTO.php
@@ -0,0 +1,44 @@
+<?php
+class UserDTO {
+	
+	var $username;
+	var $userId;
+	var $cpr;
+	var $password;
+	var $position; // kan muligvis lave vores egen enum klasse - Rùni
+	
+	function __construct($username=""){
+		return self::class;
+	}
+	
+	function getUserId(){
+		return $this->userId;
+	}
+	function setUserId($userId){
+		$this->userId = $userId;
+	}
+	
+	
+	function getUsername(){
+		return $this->username;
+	}
+	function setUsername($username){
+		$this->username = $username;
+	}
+	
+	function getCpr(){
+		return $this->cpr;
+	}
+	function setCpr($cpr){
+		$this->cpr = $cpr;	
+	}
+	
+	function getPosition(){
+		return $this->position;
+	}
+	function setPosition($position){
+		$this->position = $position;
+	}
+	
+	
+}
