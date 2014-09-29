764f84b21401cee013b77a642549907d47d760ad
Runi <Runi@82.211.200.99>
2014-09-29 20:19:24 +0200

    testclass

---
+++ RoengentProject/TestingStuff.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="StyleSheet.css">
+</head>
+    <body>
+    <p>This page uses frames. The current browser you are using does not support frames.</p>
+    <?php
+    echo "test 1";
+	$userDTO = new UserDTO("Rúni");
+	echo "test 2";
+	echo $userDTO->getUsername();
+	echo "test 3";
+	print $userDTO->getUsername();
+	?>
+    </body>
+</html>
