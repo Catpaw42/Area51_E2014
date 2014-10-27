ad30fc7656d21d3ab5596367a25d78b30e6e2699
Rúni <Rni@Morten-bb>
2014-10-27 14:53:50 +0100

    update DTO

--- Xray/src/dto/DTOPatient.java
+++ Xray/src/dto/DTOPatient.java
@@ -1,5 +1,39 @@
 package dto;
 
 public class DTOPatient {
+	
+	private String cpr;
+	private String name;
+	private String address;
+	
+	public DTOPatient(String cpr, String name, String address){
+		this.cpr = cpr;
+		this.name = name;
+		this.address = address;
+	}
+
+	public String getCpr() {
+		return cpr;
+	}
+
+	public void setCPR(String cpr) {
+		this.cpr = cpr;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
 }
--- Xray/src/dto/DTOUser.java
+++ Xray/src/dto/DTOUser.java
@@ -1,5 +1,64 @@
 package dto;
 
 public class DTOUser {
+	
+	private String username;
+	private String password;
+	private String department;
+	private String name;
+	/**
+	 * 
+	 * @param username String
+	 * @param password String
+	 * @param department String - afdeling for ansat
+	 * @param name String - er ikke nødvendig, men hvis ønsket kan den være med, som så kan bruges til at se hvem er logget på
+	 */
+	public DTOUser(String username, String password, String department, String name){
+		this.username = username;
+		this.password = password;
+		this.department = department;
+		this.name = name;
+	}
+	/**
+	 * nuværende mainconstructor da name ikke er en nødvendighed
+	 * @param username String
+	 * @param password String
+	 * @param department String
+	 */
+	public DTOUser(String username, String password, String department){
+		this(username, password, department, null);
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
