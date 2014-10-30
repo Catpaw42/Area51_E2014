0c6ffc939d6c0deb0451a24976d13c18ad57cd80
Rúni <Rni@Morten-bb>
2014-10-30 12:18:35 +0100

    Template for requisition

--- Xray/src/dto/RequisitionTemplate.java
+++ Xray/src/dto/RequisitionTemplate.java
@@ -3,10 +3,13 @@ package dto;
 import java.util.ArrayList;
 
 
-public class CheckListData {
+public class RequisitionTemplate {
 	
+
+
+
 	public static void main(String[] args) {
-		CheckListData t = new CheckListData();
+		RequisitionTemplate t = new RequisitionTemplate();
 		t.test3();
 	}
 
@@ -15,31 +18,43 @@ public class CheckListData {
 	public enum Prioritering{haste, rutine, fremskyndet, pakkeforloeb};
 	public enum AmbulantKoersel{ingen, siddende, liggende};
 	public enum IndlaeggelseTransport{gaaMedPortoer, gaaUdenPortoer, koerestol, seng};
-	public enum SaerligeForhold{hoerehaemmet, synshaemmet, amputeret, kanIkkeStaa, dement, afasi, isolation, ilt, tolk, sytostatikaDato};
-	public enum Undersoegelse{PET_CT, CT, CTKontrast, ultralyd, mr, other};
+	public enum Undersoegelsemodalitet{PET_CT, CT, CTKontrast, ultralyd, mr, other};
+	public enum Status{PENDING, CANCELED, APPROVED, DECLINED, BOOKED}
 	
 	
 	private HenvistTil henvistTil;
 	private HospitalOenske hospitalOenske;
-	private Prioritering prioritering;
-	private boolean patientSamtykke;
+	private Prioritering prioriteringOenske;
+	private boolean samtykke;
 	private boolean ambulant;
 	private String datoForslag; // skal typecheckes for korrekt dato input
 	private AmbulantKoersel ambulantKoersel;
 	private boolean indlaeggelse;
-	private IndlaeggelseTransport indlaeggelseTransport;
+	private IndlaeggelseTransport indlagtTransport;
 	private boolean pregnancy; 
 	private String cave;
-	private ArrayList<SaerligeForhold> saerligeForhold;
-	private String ilt;
+	//private ArrayList<SaerligeForhold> saerligeForhold;
+	private boolean hoerehaemmet;
+	private boolean synshaemment;
+	private boolean amputeret;
+	private boolean kanikkestaa;
+	private int iltliterprmin;
+	private String tolksprog;
+	private boolean dement;
+	private boolean afasi;
 	private String isolation;
 	private String cytostatikaDato;
-	private String tolk;
-	private Undersoegelse undersoegelse;
-	private String tidlBilleddiagnostiskUndersoegelseLokalt;
-	private String relevantUndersoegelse;
+	private Undersoegelsemodalitet undersoegelsesmodalitet;
+	private String undersoegelsestype;
+	private String tidlBilleddiagnostik;
 	private String kliniskProblemstilling;
 	private String triage;
+	private String henvlaege;
+	private String kontakttlf;
+	
+	private String visitatorPrioritering;
+	private Status status;
+	
 	
 	
 	
