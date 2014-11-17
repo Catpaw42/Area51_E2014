162eaa75f31968e62ca8e4b6cb729d835aad71c2
cbudtz <Splat1477>
2014-11-17 09:43:19 +0100

    Template for kontrolskema servlets

--- Xray/src/servlets/CTKKontrolSkemaServlet.java
+++ Xray/src/servlets/CTKKontrolSkemaServlet.java
@@ -1,6 +1,9 @@
 package servlets;
 
+import helperClasses.Const;
+
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -33,7 +36,18 @@ public class CTKKontrolSkemaServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if (request.getSession().getAttribute(Const.ACTIVE_USER)!=null){
+			process(request, response);
+		}else{
+			request.getRequestDispatcher(Const.MAIN_SERVLET)	
+			}
+		}
+
+	private void process(HttpServletRequest request,
+			HttpServletResponse response) {
 		// TODO Auto-generated method stub
+		
+	}
 	}
 
 }
